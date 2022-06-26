134183e7ce3e1ac520fc5bfc1d24a703a96468ef
AybukeAk <aybuke.hamide@outlook.com>
2022-06-26 21:18:53 +0300

    hello afrıca

--- main.py
+++ main.py
@@ -14,7 +14,3 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-git
-config - -
-global user.email
-"aybuke.hamide@outlook.com"
